c1c374b72668e26447b9e27e698f7489d4be874f
azim1993 <azimnurul1993@gmail.com>
2023-11-13 17:12:43 +0600

    Follower and Media model,migration,factory added

---
+++ Backend/app/Enums/MediaTypeEnum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+enum MediaTypeEnum: string
+{
+    case IMAGE = 'image';
+    case AUDIO = 'audio';
+    case VIDEO = 'video';
+    case DOCUMENT = 'document';
+
+    public static function toArray(): array
+    {
+        return [
+            self::IMAGE->value,
+            self::DOCUMENT->value,
+            self::AUDIO->value,
+            self::VIDEO->value,
+        ];
+    }
+}
---
+++ Backend/database/factories/MediaFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\MediaTypeEnum;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Media>
+ */
+class MediaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'src' => fake()->imageUrl,
+            'type' => fake()->randomElement(MediaTypeEnum::toArray()),
+            'mediaable_type' => null,
+            'mediaable_id' => null
+        ];
+    }
+}
