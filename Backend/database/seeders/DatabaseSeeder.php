1be279f65941930df559267773eb0611e5510145
azim1993 <azimnurul1993@gmail.com>
2023-11-13 21:50:09 +0600

    Seeder added for users and timelines

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Timeline;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)
+             ->create()
+             ->each(function($user) {
+                 Timeline::factory(5)->create(['user_id' => $user->id]);
+             });
 
     }
 }
