19a4569ebc2f20c665f4d97d285faef6fa92ad86
azim1993 <azimnurul1993@gmail.com>
2023-11-15 13:02:14 +0600

    Tailwind config set and home page added

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div class="bg-light min:h-screen">
+        <div class="flex">
+            <section class="w-3/5 h-screen flex justify-center items-center">
+                <h1 class="text-[8rem] font-bold text-secondary [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">mCast</h1>
+            </section>
+            <section class="w-2/5 flex items-center bg-secondary-light border-l-2 px-6">
+                <div class="w-full">
+                    <h2 class="text-5xl font-extralight mb-11">Happening now</h2>
+                    <div class="pb-6 border-b border-secondary">
+                        <h3 class="font-3xl font-semibold mb-5">Join toDay</h3>
+                        <a href="" class="bg-secondary text-white border inline-block border-secondary rounded-md px-5 py-2">Create New Account</a>
+                    </div>
+                    <br class="bg-secondary w-full">
+                    <div class="">
+                        <p class="mb-4">Already have account</p>
+                        <a href="" class="text-secondary border inline-block border-secondary rounded-md px-5 py-1">Login</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
 </template>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+        fontFamily: {
+            raleway: 'Raleway',
+        },
+        colors: {
+            primary: '#006d77',
+            'primary-light': '#83c5be',
+            secondary: '#e29578',
+            'secondary-light': '#ffddd2',
+            light: '#edf6f9'
+        },
+        fontSize: {
+            'xs': '.75rem',   // Extra Small
+            'sm': '.875rem',  // Small
+            'base': '1rem',   // Base
+            'lg': '1.125rem', // Large
+            'xl': '1.25rem',  // Extra Large
+            '2xl': '1.5rem',  // 2 Extra Large
+            '3xl': '1.875rem',// 3 Extra Large
+            '4xl': '2.25rem', // 4 Extra Large
+            '5xl': '3rem',    // 5 Extra Large
+            '6xl': '4rem',    // 6 Extra Large
+          },
+    },
+  },
+  plugins: [],
+}
+
